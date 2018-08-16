a136662ffb11b47e0791237a7ea4fb28dce4c1c3
naetai0412 <naetai0412@icloud.jp>
2018-08-16 13:12:00 +0000

    status

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,8 +6,8 @@
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
-        {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title') !!}
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
         
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
