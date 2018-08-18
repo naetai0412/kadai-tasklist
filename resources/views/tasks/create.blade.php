d13cbc9cfa484447e7f24a734fc47e512339adc6
naetai0412 <naetai0412@icloud.jp>
2018-08-18 13:14:46 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,24 @@
 @section('content')
 
     <h1>メッセージ新規作成ページ</h1>
-
+<div class="row">
+    <div class="col-lg-6 col-md-8 col-sm-8 col-xs-12　col-lg-offset-3 col-md-offset-2 col-sm-offset-2">
+       
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-        
+    
+      <div class="form-group">  
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+      </div>
+      
+      <div class="form-group">
         {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+      </div>
 
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-
+    </div>
+</div>
 @endsection
