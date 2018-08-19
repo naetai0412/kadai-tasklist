40b5d4787adb65e2186abb53a88ab2eaae79747a
naetai0412 <naetai0412@icloud.jp>
2018-08-19 08:46:45 +0000

    bootstrap design

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+    
+<div class="row">
+ <div class="col-lg-6 col-md-8 col-sm-8 col-xs-12　col-lg-offset-3 col-md-offset-2 col-sm-offset-2">
+
+    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+        <div class="form-group">
+           {!! Form::label('title', 'タイトル:') !!}
+           {!! Form::text('title',null,['class' => 'form-control']) !!}
+        </div>
+        
+    <div class="form-group">
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content',null, ['class' => 'form-control']) !!}
+    </div>
+
+        {!! Form::submit('更新',['class' => 'btn btn-default']) !!}
+
+    {!! Form::close() !!}
+ </div>
+</div>
+@endsection
