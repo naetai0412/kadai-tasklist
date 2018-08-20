28c252a07562fb01ddef0b582277375a23b2cacb
naetai0412 <naetai0412@icloud.jp>
2018-08-20 12:01:08 +0000

    9872

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>タスク新規作成ページ</h1>
 <div class="row">
     <div class="col-lg-6 col-md-8 col-sm-8 col-xs-12　col-lg-offset-3 col-md-offset-2 col-sm-offset-2">
        
