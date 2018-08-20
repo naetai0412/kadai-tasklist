447da36603135083ed2fd3f7da35588b382074e9
naetai0412 <naetai0412@icloud.jp>
2018-08-20 11:24:01 +0000

    112

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -45,12 +45,12 @@ class TaskController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'title' => 'required|max:191',
+            'status' => 'required|max:191',
             'content' => 'required|max:191',
         ]);
         
         $task = new Task;
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -97,12 +97,12 @@ class TaskController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'title' => 'required|max:191',
+            'status' => 'required|max:191',
             'content' => 'required|max:191',
         ]);
 
         $task = Task::find($id);
-        $task->title = $request->title;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
